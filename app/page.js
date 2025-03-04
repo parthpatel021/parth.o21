5f9a7e45777d183209093347cdfe65b445eeb719
Parth Patel <parthkumar111patel@gmail.com>
2025-03-04 08:57:03 +0530

    REF Init new project structure in Next.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="">
+        Main
+      </main>
+      <Footer />
+    </>
+  );
+}
